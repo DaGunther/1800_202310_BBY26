c37be31507be025f9bbf019ac8d179163a5bb665
takk0ss <sdoan7@my.bcit.ca>
2023-03-08 14:38:22 -0800

    added new-user.html and proofed authentication.js to include data from firestone database

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,17 @@
-function insertName() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
-
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
-            $("#name-goes-here").text(user_Name); //using jquery
-
-        } else {
-            // No user is signed in.
-        }
-    });
+function insertNameFromFirestore(){
+    // to check if the user is logged in:
+    firebase.auth().onAuthStateChanged(user =>{
+        if (user){
+           console.log(user.uid); // let me to know who is the user that logged in to get the UID
+           currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+           currentUser.get().then(userDoc=>{
+               //get the user name
+               var userName= userDoc.data().name;
+               console.log(userName);
+               //$("#name-goes-here").text(userName); //jquery
+               document.getElementById("name-goes-here").innerText=userName;
+           })    
+       }    
+    })
 }
-insertName(); //run the function
+insertNameFromFirestore();
