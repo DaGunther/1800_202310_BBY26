418ffa94e16cb0a7d7f6d6ee5b10ce88b9b9be9e
Seung Hoon Lee <jlee1095@my.bcit.ca>
2023-03-09 00:24:21 -0800

    Updated usermissions.html and main.js

--- scripts/main.js
+++ scripts/main.js
@@ -16,3 +16,54 @@ function insertNameFromFirestore(){
 }
 insertNameFromFirestore();
 
+function writeProfile() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var hikesRef = db.collection("userProfile");
+
+  hikesRef.add({
+      Name: "Sam",
+      age: "12",
+      Gender: "unassigned",
+      Height: "4ft",
+      email: "saerak@gmail.com",
+      last_updated: firebase.firestore.FieldValue.serverTimestamp(new Date("March 08, 2023"))  //current system time
+  });
+}
+
+//------------------------------------------------------------------------------
+// Input parameter is a string representing the collection we are reading from
+//------------------------------------------------------------------------------
+function displayCardsDynamically(collection) {
+  let cardTemplate = document.getElementById("profileCardTemplate");
+
+  db.collection(collection).get()   //the collection called "hikes"
+      .then(allHikes=> {
+          //var i = 1;  //Optional: if you want to have a unique ID for each hike
+          allHikes.forEach(doc => { //iterate thru each doc
+              var title = doc.data().name;       // get value of the "name" key
+              var details = doc.data().details;  // get value of the "details" key
+              var hikeCode = doc.data().code;    //get unique ID to each hike to be used for fetching right image
+              var hikeLength = doc.data().length; //gets the length field
+              var docID = doc.id;
+              let newcard = cardTemplate.content.cloneNode(true);
+
+              //update title and text and image
+              newcard.querySelector('.card-title').innerHTML = title;
+              newcard.querySelector('.card-length').innerHTML = hikeLength +"km";
+              newcard.querySelector('.card-text').innerHTML = details;
+              newcard.querySelector('.card-image').src = `./images/${hikeCode}.jpg`; //Example: NV01.jpg
+              newcard.querySelector('a').href = "eachHike.html?docID="+docID;
+              //Optional: give unique ids to all elements for future use
+              // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+              // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+              // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+              //attach to gallery, Example: "hikes-go-here"
+              document.getElementById(collection + "-go-here").appendChild(newcard);
+
+              //i++;   //Optional: iterate variable to serve as unique ID
+          })
+      })
+}
+
+displayCardsDynamically("userProfile");  //input param is the name of the collection
