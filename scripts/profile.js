5c1968a62f4e4306e8042075114a7023622638cf
Yorick Aban <yorickaban@gmail.com>
2023-03-08 21:01:14 -0800

    Added a half functional edit profile page

--- scripts/profile.js
+++ scripts/profile.js
@@ -14,3 +14,40 @@ function insertNameFromFirestore() {
     } )
 
 }
+insertNameFromFirestore();
+
+function insertCityFromFirestore() {
+    //check if user is logged in
+    firebase.auth().onAuthStateChanged( user => {
+        if ( user ) { //if user logged in
+            console.log( user.uid )
+            db.collection( "users" ).doc( user.uid ).get().then( userDoc => {
+                console.log( userDoc.data().City )
+                userCity = userDoc.data().City;
+                console.log( userName )
+                document.getElementById( "city-goes-here" ).innerHTML = userCity;
+
+            } )
+        }
+    } )
+
+}
+insertCityFromFirestore();
+
+function insertAboutFromFirestore() {
+    //check if user is logged in
+    firebase.auth().onAuthStateChanged( user => {
+        if ( user ) { //if user logged in
+            console.log( user.uid )
+            db.collection( "users" ).doc( user.uid ).get().then( userDoc => {
+                console.log( userDoc.data().About )
+                userAbout = userDoc.data().About;
+                console.log( userAbout )
+                document.getElementById( "about-goes-here" ).innerHTML = userAbout;
+
+            } )
+        }
+    } )
+
+}
+insertAboutFromFirestore();
