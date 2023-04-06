97d49eb1c8fbc948933ce313adc70654913ea3d7
Yorick Aban <yorickaban@gmail.com>
2023-04-05 20:37:54 -0700

    Added somemore text to the suggestions page

--- scripts/suggestions.js
+++ scripts/suggestions.js
@@ -61,11 +61,12 @@ function getWeather(lat, lon) {
 }
 
 function showMyFits() {
-  firebase.auth().onAuthStateChanged(user => {
+  firebase.auth().onAuthStateChanged((user) => {
     console.log("user is: " + user.uid);
-    db.collection("users").doc(user.uid)
+    db.collection("users")
+      .doc(user.uid)
       .get()
-      .then(doc => {
+      .then((doc) => {
         myposts = doc.data().outfits;
         console.log("myPosts: " + myposts);
 
@@ -80,20 +81,28 @@ function showMyFits() {
 
           // Push the promise to the promises array
           promises.push(
-            db.collection("outfits")
+            db
+              .collection("outfits")
               .doc(item)
               .get()
-              .then(doc => {
+              .then((doc) => {
                 // Check if outfit meets weather and warmth criteria
-                if (doc && doc.exists && doc.data() && doc.data().weathercondition) {
+                if (
+                  doc &&
+                  doc.exists &&
+                  doc.data() &&
+                  doc.data().weathercondition
+                ) {
                   var testField1 = doc.data().weathercondition;
                   var testField2 = doc.data().warmthlevel;
                   console.log(selectedWeather);
                   console.log(testField1);
                   console.log(selectedWarmth);
                   console.log(testField2);
-                  if (testField1 === selectedWeather
-                    && testField2 === selectedWarmth) {
+                  if (
+                    testField1 === selectedWeather &&
+                    testField2 === selectedWarmth
+                  ) {
                     console.log("Match");
                     fitoutfits.push(doc.data());
                     console.log("Fit array: " + fitoutfits[outfitIndex].name);
@@ -121,12 +130,16 @@ function displayMyFits(fitoutfits, outfitIndex) {
   // if (outfitDisplayed) return;
 
   var fitsContainer = document.getElementById("fits-go-here");
-  fitsContainer.innerHTML = '';
+  fitsContainer.innerHTML = "";
 
   var name = outfit.name;
-  console.log("Name: " + name)
-  var description = "The weather is \"" + weatherdisplay
-    + "\" and it is " + tempdisplay + " degrees Celsius. \nWe recommend this outfit.";
+  console.log("Name: " + name);
+  var description =
+    'The weather is "' +
+    weatherdisplay +
+    '" and it is ' +
+    tempdisplay +
+    " degrees Celsius. \nWe recommend this outfit.";
   var testField1 = outfit.weathercondition;
   var testField2 = outfit.warmthlevel;
   console.log("fitoutfit weather: " + outfit.weathercondition);
@@ -138,37 +151,42 @@ function displayMyFits(fitoutfits, outfitIndex) {
       var imageUrls = outfit.imageUrls;
       for (var i = 0; i < imageUrls.length; i++) {
         var imageUrl = imageUrls[i];
-        let newcard = document.getElementById("postCardTemplate").content.cloneNode(true);
-        newcard.querySelector('.card-title').innerHTML = name;
-        newcard.querySelector('.card-image').src = imageUrl;
-        newcard.querySelector('.card-description').innerHTML = description;
-        newcard.querySelector('.card').classList.add('spin'); // Add the spin class to the card
+        let newcard = document
+          .getElementById("postCardTemplate")
+          .content.cloneNode(true);
+        newcard.querySelector(".card-title").innerHTML = name;
+        newcard.querySelector(".card-image").src = imageUrl;
+        newcard.querySelector(".card-description").innerHTML = description;
+        newcard.querySelector(".card").classList.add("spin"); // Add the spin class to the card
         fitsContainer.appendChild(newcard);
       }
       outfitDisplayed = true;
     } else {
-      console.log('imageUrls property not found');
+      console.log("imageUrls property not found");
     }
   } else {
     console.log(selectedWeather);
     console.log(selectedWarmth);
     console.log(imageUrl);
-    console.log('That drip doesnt fit');
+    console.log("That drip doesnt fit");
   }
 }
-
 
 const myButton = document.getElementById("button1");
 const myButton2 = document.getElementById("button2");
 const loadingMsg = document.getElementById("loading");
 const loadingMsg2 = document.getElementById("loading1");
+const msg1 = document.getElementById("suggestion");
 myButton2.style.display = "none";
 
 document.getElementById("button1").addEventListener("click", function () {
   // hide button1
+  
   myButton.style.display = "none";
+  
 
   // show "loading" message
+
   loadingMsg.style.display = "block";
 
   // delay the getLocation() function call by 2 seconds
@@ -184,28 +202,29 @@ document.getElementById("button1").addEventListener("click", function () {
 
       // show button2
       setTimeout(function () {
+        msg1.style.display = "block";
         myButton2.style.display = "block";
-
-      }, 3000);
-    }, 3000);
-  }, 3000);
-
+      }, 2000);
+    }, 2000);
+  }, 2000);
 });
 
 const cardSuggestion = document.getElementById("fits-go-here");
+const suggestion = document.getElementById("");
 
 document.getElementById("button2").addEventListener("click", function () {
   outfitDisplayed = false; // Reset the outfitDisplayed flag
   // cardSuggestion.style.display = "none";
   loadingMsg.style.display = "block";
-  
+  msg1.style.display = "none";
+
+
   // delay the getLocation() function call by 2 seconds
   setTimeout(function () {
     loadingMsg.style.display = "none";
     loadingMsg2.style.display = "block";
     setTimeout(function () {
       loadingMsg2.style.display = "none";
-      
 
       outfitIndex++;
       displayMyFits(fitoutfits, outfitIndex);
@@ -215,8 +234,9 @@ document.getElementById("button2").addEventListener("click", function () {
       // show button2
       setTimeout(function () {
         myButton2.style.display = "block";
+        msg1.style.display = "block";
 
-      }, 3000);
-    }, 3000);
-  }, 3000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
 });
